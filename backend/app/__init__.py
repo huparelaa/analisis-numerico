68a937ff1db2c56d743e5a9298c8a32a201084e2
Hobar <huparelaa@eafit.edu.co>
2023-11-17 11:50:43 -0500

    fix: cors problem solved

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,8 +2,11 @@ from flask import Flask
 from app.routes_non_linear import api_non_linear
 from app.routes_linear import api_linear
 from app.routes_interpolation import api_interpolation
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+CORS(app)
 
 app.register_blueprint(api_non_linear, url_prefix='/non-linear')
 app.register_blueprint(api_linear, url_prefix='/linear')
